af085dc144f824f288fce11450952a30626c1332
srisarvesh <r.srisarvesh@gmail.com>
2021-07-30 15:14:28 +0530

    After code refactoring we just put the code where we wrote about the form into a signupForm function and we call that signupForm function from the render() with the values of the state as a parameters

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -54,20 +54,8 @@ class Signup extends Component {
    })
    .catch(error =>console.log(error));//if the request was not successful 
   }
-
-  render() {
-    const {name, email, password,error,open}=this.state;
-    return (
-        <div className="container">{/* we are using the bootstrap classes as we already kept the cdn link of the bootstrap in our index.html page*/}
-          <h2 className="mt-5 mb-5">Signup</h2>{/* this will give a bit of padding in bottom and up*/}
-
-          <div className="alert alert-primary" style={{display:error?"":"none"}}>
-            { error}
-          </div>
-          <div className="alert alert-primary" style={{display:open?"":"none"}}>
-             New Account created successfully please signin.
-          </div>
-          <form>
+  sighupForm=(name,email,password)=>(
+    <form>
             <div className="form-group">
               <lable className="text-muted"> Name</lable>
               <input onChange={this.handleChange("name")}  type="text" className="form-control" value={name}/>{/* this form-Control class helps in proving the the connection between the user entered value and the server*/}
@@ -81,7 +69,21 @@ class Signup extends Component {
               <input onChange={this.handleChange("password")} type="password" className="form-control"  value={password} />{/* this form-Control class helps in proving the the connection between the user entered value and the server*/}
             </div>
             <button  className="btn btn-raised btn-success" onClick={this.clickSubmit}> Submit</button>
-          </form>{/*for each input field we have value=this.state.Inputfieldname  that is because what ever value that you enter in the input field that will be get updated in the state and also here it will .so this is called controlled componnents*/}
+  </form>//*for each input field we have value=this.state.Inputfieldname  that is because what ever value that you enter in the input field that will be get updated in the state and also here it will .so this is called controlled componnents*/}
+  );
+  render() {
+    const {name, email, password,error,open}=this.state;
+    return (
+        <div className="container">{/* we are using the bootstrap classes as we already kept the cdn link of the bootstrap in our index.html page*/}
+          <h2 className="mt-5 mb-5">Signup</h2>{/* this will give a bit of padding in bottom and up*/}
+
+          <div className="alert alert-danger" style={{display:error?"":"none"}}>
+            { error}
+          </div>
+          <div className="alert alert-primary" style={{display:open?"":"none"}}>
+             New Account created successfully please signin.
+          </div>
+          {this.sighupForm(name,email,password)}
         </div>
     );
   }
