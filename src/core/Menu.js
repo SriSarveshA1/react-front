e437ebe3e68971b150811ba6c81de131375d1c7a
srisarvesh <r.srisarvesh@gmail.com>
2021-07-31 08:53:20 +0530

    Implementing sigout part along the same nav tab so when we click the signout the jwt stored in the local storage will be removed and also callback will happpen and redirect will be performed

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -12,6 +12,27 @@ const isActive =(history,path) => {
 
   }
 
+export const signout =(callback) => {
+    //callback function that we want to get executed soon after we remove the jwt from the local storage
+    if(typeof window!=="undefined")
+    {
+        //so the window object must be available to remove the jwt from the local storage 
+        window.localStorage.removeItem("jwt");//we remove the jwt key value pair
+        callback();//as soon as we remove the jwt from the local storage we want the redirection page to be happen to home page /login page
+        //after this we send response to the backend
+        return fetch("http://localhost:8080/signout",{
+            method:"GET"
+        })
+        //so if the sending get request to the backend went fine then we call the then method
+        .then((res)=> {//here we send the response in the json format
+          console.log("signout",res);
+          return res.json();
+        })
+        .catch((err)=>console.log(err));//if there is error occured we just console log it
+        
+    }
+
+}
 
 
 
@@ -27,9 +48,12 @@ const Menu=({history})=>( //here from the props that we got from the withRouter(
           <li class="nav-item">
               <Link className="nav-link" style={isActive(history,"/signup")}  to="/signup">Signup</Link>
           </li>
+          <li class="nav-item">
+              <a className="nav-link" style={(isActive(history,"/signout"),{cursor:"pointer",color:"#fff"})} onClick={() =>signout(()=>history.push("/"))} >Signout</a>
+          </li>
            
          </ul>      
-    </div>
+    </div>  
 )
 
 
