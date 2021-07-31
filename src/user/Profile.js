6a40c52bd1c4026d5ae0542381e0566b6eae6f0f
srisarvesh <r.srisarvesh@gmail.com>
2021-07-31 15:33:01 +0530

    After getting the users info from the local storage

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,10 +1,23 @@
 import React,{Component} from "react";
-
+import {isAuthenticated} from "../auth";
 class Profile extends Component{
+    constructor()
+    {
+        super();
+        this.state = {
+            user:"",
+            redirectToReferer:false//we have this bec
+        }
+    }
+    componentDidMount(){
+        console.log("user id fromt the router parameter",this.props.match.params.userId);
+    }
     render() {
         return (
            <div className="Container">
                <h2 className="mt-5 mb-5">Profile</h2>
+               <p>Hello {isAuthenticated().user.name}</p>
+               <p>Email {isAuthenticated().user.email}</p>
            </div>
         );
     }
