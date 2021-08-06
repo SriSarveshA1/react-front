c697c61c8c4c050fdba7bac180b60a5e0f7f828b
srisarvesh <r.srisarvesh@gmail.com>
2021-08-06 08:32:43 +0530

    reser  password frontend

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import ResetPassword from "./user/ResetPassword";
 import Home from "./core/Home";
 import Menu from "./core/Menu";
 import Signup from "./user/Signup";
@@ -20,6 +21,7 @@ const MainRouter = () => (
         <Menu />
         <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/reset-password/:resetPasswordToken" component={ResetPassword}/>
             <PrivateRoute exact path="/post/create" component={NewPost} />
             <Route exact path="/post/:postId" component={SinglePost} />
             <Route exact path="/forgot-password" component={ForgotPassword} />
