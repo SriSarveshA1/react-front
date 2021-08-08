94c5a27b0aea2f3786b92398672e63b195481a0d
srisarvesh <r.srisarvesh@gmail.com>
2021-08-08 17:27:18 +0530

    After final changes 7

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
 import Menu from "./core/Menu";
 import Signup from "./user/Signup";
-import Signin from "./user/Signin.js";
+import Signin from "./src/user/Signin.js";
 import Profile from "./user/Profile";
 import Users from "./user/Users";
 import EditProfile from "./user/EditProfile";
