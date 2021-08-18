d144a4ae7eec0495f37572005804ec7f28164618
srisarvesh <r.srisarvesh@gmail.com>
2021-08-18 10:46:01 +0530

    Deploy 9

--- src/post/components/EditPost.js
+++ src/post/components/EditPost.js
@@ -170,11 +170,9 @@ class EditPost extends Component {
                     alt={title}
                 />
 
-               {isAuthenticated().user.role === "admin" &&
-                    this.editPostForm(title, body)}
-
-                {isAuthenticated().user._id === id &&(isAuthenticated().user.role!="admin")&&
-                    this.editPostForm(title, body)}
+                 {isAuthenticated().user.role === "admin" ||
+                    (isAuthenticated().user._id === id &&
+                        this.editPostForm(title, body))}
             </div>
         );
     }
